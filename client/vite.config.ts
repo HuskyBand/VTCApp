06a0536f25fbf675ec1d7fe6bd2abb6f94c48d56
jgarbyte <398utubzyt@gmail.com>
2026-07-15 01:32:55 -0700

    Fix imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,13 +2,14 @@ import {fileURLToPath} from 'node:url';
 import path, {dirname} from 'node:path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { DEFAULT_BASE_API_URL } from '../packages/api/src/Constants.ts';
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  	plugins: [react()],
 	resolve: {
 		alias: [
 			{find: '@client', replacement: path.resolve(__dirname, 'src')},
@@ -17,4 +18,11 @@ export default defineConfig({
 		],
 		extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js', '.json'],
 	},
+	server: {
+		cors: {
+			origin: DEFAULT_BASE_API_URL,
+			methods: ['GET', 'PUT', 'POST', 'DELETE'],
+			allowedHeaders: ['Content-Type', 'Authorization']
+		}
+	}
 })
