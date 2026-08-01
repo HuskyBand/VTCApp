a9f6deb7cfe001ebfe4404a5eb006657710dde30
jgarbyte <398utubzyt@gmail.com>
2026-08-01 01:47:48 -0700

    Begin work on UI overhaul for mobile support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import {fileURLToPath} from 'node:url';
 import path, {dirname} from 'node:path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { DEFAULT_BASE_API_URL } from '../packages/api/src/Constants.ts';
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -17,12 +16,5 @@ export default defineConfig({
 			{find: '~', replacement: path.resolve(__dirname, 'src')},
 		],
 		extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js', '.json'],
-	},
-	server: {
-		cors: {
-			origin: DEFAULT_BASE_API_URL,
-			methods: ['GET', 'PUT', 'POST', 'DELETE'],
-			allowedHeaders: ['Content-Type', 'Authorization']
-		}
 	}
 })
